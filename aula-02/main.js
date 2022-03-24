2b5d8f1f2c40ac32b952a257732e1dc3cc763357
João Felipi Cardoso <joaofelipi877@gmail.com>
2022-03-24 11:26:13 -0300

    Aula 02 finalizada

--- aula-02/main.js
+++ aula-02/main.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("submit").addEventListener("click", calculate);
-const result  = document.getElementById("result")
+const result  = document.getElementById("result");
 
 function calculate() {
 
@@ -7,27 +7,28 @@ function calculate() {
   let valorDeB = document.getElementById("VB");
   let valorDeC = document.getElementById("VC");
 
-  let res = baskara(valorDeA.value, valorDeB.value, valorDeC.value)
+  let res = bhaskara(valorDeA.value, valorDeB.value, valorDeC.value);
 
-  res ? result.innerHTML = `X1: ${res.x1.toFixed(3)}, X2: ${res.x2.toFixed(3)}` : result.innerHTML = "Inexistentes!"
+  res ? result.innerHTML = `X1: ${res.x1.toFixed(3)}, <br/> X2: ${res.x2.toFixed(3)}, <br/> Delta: ${res.delta}` : result.innerHTML = "Inexistentes!";
 
 };
 
 
-function baskara(a, b, c) {
-  const delta = (b*b) -( 4 * a * c)
+function bhaskara(a, b, c) {
+  const delta = (b*b) -( 4 * a * c);
 
   if (delta < 0) {
 
    return false;
 
-  } ;
+  };
 
   let result = {
       x1: (-b + Math.sqrt(delta)) / (2 * a),
-      x2: (-b - Math.sqrt(delta)) / (2 * a)
+      x2: (-b - Math.sqrt(delta)) / (2 * a),
+      delta: delta
   };
 
-  return result
+  return result;
 
 }
