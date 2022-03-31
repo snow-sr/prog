26ef96a24c51ef31d00903319df7fa8e5e825d28
João Felipi Cardoso <joaofelipi877@gmail.com>
2022-03-31 11:33:20 -0300

    Todo complete

--- todo/To-do List/main.js
+++ todo/To-do List/main.js
@@ -5,34 +5,49 @@ const solved = [];
 
 function createNewTask(){
   let taskToBeCreated = document.getElementById("InputTask").value;
+
+  if(taskToBeCreated){
+    
   let newTask = {
     "message": taskToBeCreated,
     "solved": false,
-    "id": taskToBeCreated.split(" ")[0]
+    "id": "task" + Date.now(),
   };
 
   let task = tasks.push(newTask);
-  syncTask(tasks[task-1]);
+
+  syncTask(tasks[task-1]);  
+
+  document.getElementById("InputTask").value = ""
+
+  };
+
 };
 
 function syncTask(task){
 
+  if(task.solved){
+    list.querySelector(`#${task.id}`).remove();
+  }
+
   if(!task.solved){
   list.innerHTML += `<li class="hover:text-red-500" id="${task.id}" onclick="removeTask(${task.id})">${task.message}</id>`
-  };
+  }
 
 
 };
 
 function removeTask(taskId){ 
-  console.log(taskId.id)
-  let findTask = tasks.find(a => a.id === taskId.id)
-  console.log(findTask)
+  
+  let id = taskId.id;
+
+  let findTask = tasks.find(tsk => tsk.id === id)
+
   findTask.solved = true;
   solved.push(findTask)
+  syncTask(findTask)
   
   let remove = tasks.indexOf(findTask)
   tasks.splice(remove, 1)
 
-  console.table(tasks)
 }
