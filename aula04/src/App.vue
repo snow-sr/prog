f4185a3ae5f45b097f1f65df9bb8f8d8a3cea277
João Felipi Cardoso <joaofelipi877@gmail.com>
2022-04-14 11:43:47 -0300

    Aula 04

---
+++ aula04/src/App.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      theme: true,
+    };
+  },
+
+  methods: {
+    changeTheme: () => {
+      if (this.theme == false) {
+        let toTurnDark = document.querySelectorAll("#canTurnDark");
+        toTurnDark.forEach((element) => {
+          element.classList.add("dark");
+        });
+      }
+
+      let toTurnWhite = document.querySelectorAll("#canTurnDark");
+      toTurnWhite.forEach((element) => {
+        element.classList.remove("dark");
+      });
+    },
+  },
+};
+</script>
+<template>
+  <main id="canTurnDark">
+    <div class="header">
+      <p class="title">"HAHAHA"</p>
+    </div>
+
+    <div class="body" id="canTurnDark">
+      <p class="citation">
+        "HAHAHAHA HAHAHAHA HAHAHAHA HAHAHAHA HAHAHAHA <br />
+        HAHAHAHA HAHAHAHA HAHAHAHA HAHAHAHA HAHAHAHA <br />
+        HAHAHAHA HAHAHAHA HAHAHAHA HAHAHAHA HAHAHAHA" <br />
+      </p>
+    </div>
+
+    <div id="canTurnDark">
+      <p class="author" @click="changeTheme()">Joker, The, 2022</p>
+    </div>
+  </main>
+</template>
